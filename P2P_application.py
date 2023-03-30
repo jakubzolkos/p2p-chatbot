b6eb0e7230018885fd9c04d77db0d5f1f8f677ba
isshaddad <issh@bu.edu>
2023-03-30 09:50:20 -0400

    1 on 1 chat system(internally and via network)

--- P2P_application.py
+++ P2P_application.py
@@ -1,51 +1,33 @@
 import socket
-import threading
 
-class Chatbot:
-    def __init__(self, ip, port):
-        self.ip = ip
-        self.port = port
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.users = ['10.239.208.17']
-        self.sessions = {}
+# Define host and port number
+host = 'localhost'
+port = 12345
 
-    def start_session(self, ip):
-        self.sessions[ip] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sessions[ip].connect((ip, self.port))
-        self.sessions[ip].send(f"Connected to {self.ip}".encode())
+# Create socket object
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def handle_client(self, client_socket, address):
-        self.sessions[address[0]] = client_socket
-        print(f"Connected to {address[0]}")
-        while True:
-            message = client_socket.recv(1024).decode()
-            if message:
-                print(f"{address[0]}: {message}")
+# Bind socket to host and port
+s.bind((host, port))
 
-    def listen(self):
-        self.sock.bind((self.ip, self.port))
-        self.sock.listen()
-        while True:
-            conn, addr = self.sock.accept()
-            self.sessions[addr[0]] = conn
-            threading.Thread(target=self.handle_client, args=(conn,)).start()
+# Listen for incoming connections
+s.listen()
 
-    def broadcast_message(self, message):
-        for user in self.users:
-            if user != self.ip: # Skip sending message to own IP address
-                self.sessions[user].send(f"{self.ip}: {message}".encode())
+# Wait for connection from client
+client, addr = s.accept()
+print(f'Connected to {addr}')
 
+# Loop to receive and send messages
+while True:
+    # Receive message from client
+    message = client.recv(1024).decode()
 
-if __name__ == "__main__":
+    # Check if message is not empty
+    if message:
+        print(f'Received: {message}')
 
-    chatbot = Chatbot('10.239.208.17', 5000)
+        # Send message back to client
+        client.send(f'You said: {message}'.encode())
 
-    for user in chatbot.users:
-        if user != chatbot.ip:
-            chatbot.start_session(user)
-
-    threading.Thread(target=chatbot.listen).start()
-
-    while True:
-        message = input()
-        chatbot.broadcast_message(message)
+# Close connection
+client.close()
